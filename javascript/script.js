f1ab0955c70cc4e9e1fe02ddcdb5cb7a6c77e3a1
SyariefHayat <muhammadsyarifhayat@gmail.com>
2024-02-19 17:46:45 +0700

    Menambahkan 3 file baru untuk menyimpan homepage quiz

--- javascript/script.js
+++ javascript/script.js
@@ -36,3 +36,52 @@ function nextSlide() {
   slider.prepend(slides[currentSlide - 1]);
   currentSlide--;
 }
+
+const overStreak = document.querySelector(".streak");
+const overHeart = document.querySelector(".heart");
+const overPoints = document.querySelector(".points");
+
+
+const streak = document.querySelector("li:nth-child(1)");
+streak.addEventListener("click", () => {
+  overStreak.classList.toggle("active");
+  overHeart.classList.remove("active");
+  overPoints.classList.remove("active");
+});
+
+const heart = document.querySelector("li:nth-child(2)");
+heart.addEventListener("click", () => {
+  overHeart.classList.toggle("active");
+  overStreak.classList.remove("active");
+  overPoints.classList.remove("active");
+});
+
+const points = document.querySelector("li:nth-child(3)");
+points.addEventListener("click", () => {
+  overPoints.classList.toggle("active");
+  overStreak.classList.remove("active");
+  overHeart.classList.remove("active");
+});
+
+// FUNGSI UNTUK MENGHAPUS CLASS TANPA MENEKAN LI
+document.addEventListener("click", (e) => {
+  const isClickInsideStreak = streak.contains(e.target);
+  const isClickInsideOverStreak = overStreak.contains(e.target);
+  const isClickInsideHeart = heart.contains(e.target);
+  const isClickInsideOverHeart = overHeart.contains(e.target);
+  const isClickInsidePoints = points.contains(e.target);
+  const isClickInsideOverPoints = overPoints.contains(e.target);
+
+  if (!isClickInsideStreak && !isClickInsideOverStreak) {
+    overStreak.classList.remove("active");
+  };
+
+  if (!isClickInsideHeart && !isClickInsideOverHeart) {
+    overHeart.classList.remove("active");
+  };
+
+  if (!isClickInsidePoints && !isClickInsideOverPoints) {
+    overPoints.classList.remove("active");
+  };
+
+});
